b9e4b5eb8673fa4eec3bbfe86e984187b670a2e8
Nathalia Silva Lima <nathaliasilvalima5@gmail.com>
2023-06-06 12:29:43 -0300

    Início controle de versao

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,19 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+
+		
+		private long id;
+		private String nome;
+		private String descrição;
+		private String codigoBarras;
+		private double preco;
+		
+		// public: Acesso liberado para todas as classes
+		
+		// private : Acesso permitido apenas para os membros da própria classe.
+		// Entende-se por membros(atributos e ou métodos)
+
+	}
+
+
